0faac204ac76897135f7502c9157d1f2b6465e23
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-03-19 17:57:34 +0900

    Update Review_task_20240314.py

--- week2/Review_task_20240314.py
+++ week2/Review_task_20240314.py
@@ -13,18 +13,21 @@ hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 h, s, v = cv.split(hsv)
 
 # 1 Change the hue by 180 degrees
-h1 = (h + 90) % 180 
+h1 = np.clip((h + 90) % 180, 0, 255).astype(hsv.dtype) 
 hue = cv.merge([h1, s, v])
+hue = np.uint8(hue)
 hue = cv.cvtColor(hue, cv.COLOR_HSV2BGR)
 
 # 2 Decrease the saturation by 50%
 s1 = np.clip(s * 0.5, 0, 255).astype(hsv.dtype)
 saturation = cv.merge([h, s1, v])
+saturation = np.uint8(saturation)
 saturation = cv.cvtColor(saturation, cv.COLOR_HSV2BGR)
 
 # 3 Increase the value by 50%
 v1 = np.clip(v * 1.5, 0, 255).astype(hsv.dtype)
 value = cv.merge([h, s, v1])
+value = np.uint8(value)
 value = cv.cvtColor(value, cv.COLOR_HSV2BGR)
 
 # Merge 2x2 images into a single image
